48aaa58444960e2612dbbf49707e3ab4672a9c0b
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2019-07-17 14:16:13 +0200

    Better link detection

--- src/subparsers/stripLinkDefinitions.js
+++ src/subparsers/stripLinkDefinitions.js
@@ -7,14 +7,20 @@ import encodeAmpsAndAngles from './encodeAmpsAndAngles';
  */
 export default function stripLinkDefinitions (text, globals) {
 
-  let regex       = new RegExp('^ {0,'+globals.tabWidthLimit+'}\\[(.+)]:[ \t]*\n?[ \t]*<?([^>\\s]+)>?(?: =([*\\d]+[A-Za-z%]{0,4})x([*\\d]+[A-Za-z%]{0,4}))?[ \t]*\n?[ \t]*(?:(\n*)["|\'(](.+?)["|\')][ \t]*)?(?:\n+|(?=¨0))', 'gm'),
-      base64Regex = new RegExp('^ {0,'+globals.tabWidthLimit+'}\\[(.+)]:[ \t]*\n?[ \t]*<?(data:.+?\/.+?;base64,[A-Za-z0-9+/=\n]+?)>?(?: =([*\\d]+[A-Za-z%]{0,4})x([*\\d]+[A-Za-z%]{0,4}))?[ \t]*\n?[ \t]*(?:(\n*)["|\'(](.+?)["|\')][ \t]*)?(?:\n\n|(?=¨0)|(?=\n\\[))', 'gm');
+  let regex       = new RegExp('^ {0,'+globals.tabWidthLimit+'}\\[([^\\]]+)]:[ \t]*\n?[ \t]*<?([^>\\s]+)>?(?: =([*\\d]+[A-Za-z%]{0,4})x([*\\d]+[A-Za-z%]{0,4}))?[ \t]*\n?[ \t]*(?:(\n*)["|\'(](.+?)["|\')][ \t]*)?(?:\n+|(?=¨0))', 'gm'),
+      base64Regex = new RegExp('^ {0,'+globals.tabWidthLimit+'}\\[([^\\]]+)]:[ \t]*\n?[ \t]*<?(data:.+?\/.+?;base64,[A-Za-z0-9+/=\n]+?)>?(?: =([*\\d]+[A-Za-z%]{0,4})x([*\\d]+[A-Za-z%]{0,4}))?[ \t]*\n?[ \t]*(?:(\n*)["|\'(](.+?)["|\')][ \t]*)?(?:\n\n|(?=¨0)|(?=\n\\[))', 'gm');
 
   // attacklab: sentinel workarounds for lack of \A and \Z, safari\khtml bug
   text += '¨0';
 
   let replaceFunc = function (wholeMatch, linkId, url, width, height, blankLines, title) {
+    
+    // if there aren't two instances of linkId it must not be a reference link so back out
     linkId = linkId.toLowerCase();
+    if (text.toLowerCase().split(linkId).length - 1 < 2) {
+      return wholeMatch;
+    }
+
     if (url.match(/^data:.+?\/.+?;base64,/)) {
       // remove newlines
       globals.gUrls[linkId] = url.replace(/\s/g, '');
